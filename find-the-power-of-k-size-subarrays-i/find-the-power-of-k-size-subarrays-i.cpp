a8dcfd98e7c7d2b8092b69dc407cf1c09e77c8af
Shashwat Singh <123892669+Shashron7@users.noreply.github.com>
2024-11-16 12:20:39 +0530

    feat: update cpp solution for find-the-power-of-k-size-subarrays-i

--- find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
-    // bool check(vector<int>& nums)
-    // {
-    //     for(int i=1; i<nums.size(); i++)
-    //     {
-    //         if(nums[i]-nums[i-1]!=1) return false;
-    //     }
-    //     return true;
-    // }
+   
     vector<int> resultsArray(vector<int>& nums, int k)
     {
        int len=0;
-       int l=0;
        vector<int> ans;
        for(int r=0; r<nums.size(); r++)
        {
